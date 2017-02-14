eab5d05afff7c8b2b6b3ab5bbd6e4e69944fc85a
Mattia <guzzi.mattia@gmail.com>
2017-02-14 17:03:50 +0100

    no message

---
+++ extras/init.php
@@ -0,0 +1,16 @@
+<?php
+	$extra_includes = [
+		'extras/scripts.php',
+        'extras/styles.php',
+        'extras/setup.php',
+        'extras/utils.php',
+	];
+
+foreach ($extra_includes as $file) {
+  if (!$filepath = locate_template($file)) {
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+  }
+
+  require_once $filepath;
+}
+unset($file, $filepath);
