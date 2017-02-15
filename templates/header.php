7590ad632bce610b0f3cc44cb7aa55bb9e554781
Mattia <guzzi.mattia@gmail.com>
2017-02-15 16:39:00 +0100

    no message

--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,10 @@
 <header class="header">
-  <a class="header__logo">
-
+  <a class="header__logo icon icon-pittogramma" href="<?= esc_url(home_url('/')); ?>">
   </a>
   <nav class="header__nav">
+      <div class="header__catalog">
+          <a class="catalog_btn" href="#"><?php _e('Catalogo','verga') ?></a>
+          </div>
     <?php
     if (has_nav_menu('primary_navigation')) :
      /* wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'walker' => 'walker_texas_ranger']);*/
