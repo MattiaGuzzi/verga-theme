9372e63d80eae55be446e537c1b4a0a64eb6961c
Mattia <guzzi.mattia@gmail.com>
2017-02-15 17:50:20 +0100

    no message

--- templates/footer.php
+++ templates/footer.php
@@ -4,7 +4,7 @@
   </div>
   <?php
   if (has_nav_menu('footer_navigation')) :
-    bem_menu('footer_navigation', 'menu');
+    bem_menu('footer_navigation', 'menu__footer');
   endif;
   ?>
 </footer>
