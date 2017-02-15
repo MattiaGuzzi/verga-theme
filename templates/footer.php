788143dfa913a56eb6f8ecc63f7cd0c6bd8a68b4
Mattia <guzzi.mattia@gmail.com>
2017-02-15 17:40:56 +0100

    no message

--- templates/footer.php
+++ templates/footer.php
@@ -2,4 +2,10 @@
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+  <?php
+  if (has_nav_menu('footer')) :
+    /* wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'walker' => 'walker_texas_ranger']);*/
+    bem_menu('footer', 'menu');
+  endif;
+  ?>
 </footer>
