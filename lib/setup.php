2686b868ee36a8dc20279ce88da92cd0bffa1dcd
Mattia <guzzi.mattia@gmail.com>
2017-02-15 17:33:09 +0100

    no message

--- lib/setup.php
+++ lib/setup.php
@@ -101,3 +101,7 @@ function assets() {
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
+/* add footer menu */
+register_nav_menus([
+    'footer_navigation' => __('footer', 'sage')
+]);
