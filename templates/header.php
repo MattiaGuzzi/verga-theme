53e114b1d54223dd373a2cb0ac0f0d5ab8e04b43
Mattia <guzzi.mattia@gmail.com>
2017-02-15 12:02:45 +0100

    header

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,17 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+<header class="header">
+  <div class="header__logo">
+
+  </div>
+  <nav class="header__nav">
+    <?php
+    if (has_nav_menu('primary_navigation')) :
+      wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+    endif;
+    ?>
+  </nav>
+  <div class="header__preventivi">
+    <a href="#"  class="button">
+     <?php _e('Preventivo', 'verga') ?>
+    </a>
   </div>
 </header>
