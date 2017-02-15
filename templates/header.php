365092d9dbb6890cdd681027bdd9c7f4f5024340
Mattia <guzzi.mattia@gmail.com>
2017-02-15 16:49:02 +0100

    no message

--- templates/header.php
+++ templates/header.php
@@ -1,20 +1,20 @@
-<header class="header">
-  <a class="header__logo icon icon-pittogramma" href="<?= esc_url(home_url('/')); ?>">
-  </a>
-  <nav class="header__nav">
-      <div class="header__catalog">
-          <a class="catalog_btn" href="#"><?php _e('Catalogo','verga') ?></a>
-          </div>
-    <?php
-    if (has_nav_menu('primary_navigation')) :
-     /* wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'walker' => 'walker_texas_ranger']);*/
-   bem_menu('primary_navigation', 'menu');
-    endif;
-    ?>
-  </nav>
-  <div class="header__preventivi">
-    <a href="#"  class="button">
-     <?php _e('Preventivo', 'verga') ?>
+<header class="header header--shrink">
+    <a class="header__logo icon icon-pittogramma" href="<?= esc_url(home_url('/')); ?>">
     </a>
-  </div>
+    <nav class="header__nav">
+
+        <a class="button--catalog" href="#"><?php _e('Catalogo', 'verga') ?></a>
+
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+            /* wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'walker' => 'walker_texas_ranger']);*/
+            bem_menu('primary_navigation', 'menu');
+        endif;
+        ?>
+    </nav>
+    <div class="header__preventivi">
+        <a href="#" class="button">
+            <?php _e('Preventivo', 'verga') ?>
+        </a>
+    </div>
 </header>
