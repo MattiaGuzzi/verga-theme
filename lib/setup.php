86b4514f317f293f1583b51c91a0171e51baf193
Mattia <guzzi.mattia@gmail.com>
2017-02-15 12:17:17 +0100

    no message

--- lib/setup.php
+++ lib/setup.php
@@ -96,11 +96,7 @@ function display_sidebar() {
  */
 function assets() {
   wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
-
-  if (is_single() && comments_open() && get_option('thread_comments')) {
-    wp_enqueue_script('comment-reply');
-  }
-
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+  wp_enqueue_script( 'lib', Assets\asset_path('scripts/lib.js'), null, null, true );
+  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), 'lib', null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
