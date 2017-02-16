1a1ac0670a8dd55b20f7224fe419518f75f6483d
Mattia <guzzi.mattia@gmail.com>
2017-02-16 14:32:11 +0100

    no message

---
+++ builder/fascia-normal.php
@@ -0,0 +1,6 @@
+<section class="divider<?php  echo (get_sub_field('colore_sfondo')=='red') ? ' divider--enphasis' : '' ?>">
+    <h2 class="divider__title title--shrink title--grow-md"><?php the_sub_field('titolo'); ?></h2>
+    <div class="divider__paragraph paragraph--shrink paragraph--grow-md"><?php the_sub_field('paragrafo'); ?></div>
+    <div class="divider__button button--shrink button--grow-md"><a href="<?php the_sub_field('link_pagina') ?>" class="<?php  echo (get_sub_field('colore_sfondo')=='red') ? ' button--reverse' : ' button' ?>"><?php the_sub_field('testo_bottone') ?></a>
+    </div>
+</section>
