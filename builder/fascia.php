7b689e0affeb1bb8cac3150ee5e1ea941f157e81
Mattia <guzzi.mattia@gmail.com>
2017-02-16 14:36:59 +0100

    no message

--- builder/fascia.php
+++ builder/fascia.php
@@ -1,2 +1,3 @@
-<?php include(locate_template('builder/'.get_row_layout().'-'. get_sub_field('fascia_full') ? 'full.php':'normal.php', false, false)) ?>
+<?php $file =  get_sub_field('fascia_full') ? 'full.php':'normal.php';
+include(locate_template('builder/'.get_row_layout().'-'.$file, false, false)) ?>
 
