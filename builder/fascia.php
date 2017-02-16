2c87fcfc8fcb5b744bad64c2d75d65b0d244b189
Mattia <guzzi.mattia@gmail.com>
2017-02-16 12:15:15 +0100

    no message

--- builder/fascia.php
+++ builder/fascia.php
@@ -1,4 +1,4 @@
-<section class="divider">
+<section class="divider<?php  echo (get_sub_field('colore_sfondo')=='red') ? ' divider--red' : '' ?>">
     <h2><?php the_sub_field('titolo'); ?></h2>
     <p><?php the_sub_field('paragrafo'); ?></p>
     <a href="<?php the_sub_field('link_pagina') ?>" class="<?php  echo (get_sub_field('colore_sfondo')=='red') ? 'button' : 'button--reverse' ?>"><?php the_sub_field('testo_bottone') ?></a>
