975260c63845795268b67f601468b2a6ede46a46
Mattia <guzzi.mattia@gmail.com>
2017-02-16 16:47:02 +0100

    no message

--- builder/catalogo.php
+++ builder/catalogo.php
@@ -1,19 +1,23 @@
 <section class="catalog">
-<?php
-$args = array(
-    'orderby' => 'name',
-    'hide_empty' => 0,
-    'parent' => 0
-);
-$product_categories = get_terms( 'product_cat', $args );
-foreach ( $product_categories as $product_category ) {
-   echo $product_category -> name;
-
+    <?php
     $args = array(
         'orderby' => 'name',
         'hide_empty' => 0,
-        'parent' => $product_category -> term_id,
+        'parent' => 0
     );
-}
-?>
+    $product_categories = get_terms('product_cat', $args);
+    foreach ($product_categories as $product_category) {
+        echo $product_category->name;
+
+        $args = array(
+            'orderby' => 'name',
+            'hide_empty' => 0,
+            'parent' => $product_category->term_id,
+        );
+        $subcategories = get_terms('product_cat', $args);
+        foreach ($subcategories as $subcategory) {
+            echo $subcategory->name;
+        }
+    }
+    ?>
 </section>
