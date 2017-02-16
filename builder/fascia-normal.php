e46792aeecf38ff283ccba33c6694d5c0c0f53c6
Mattia <guzzi.mattia@gmail.com>
2017-02-16 15:06:19 +0100

    no message

--- builder/fascia-normal.php
+++ builder/fascia-normal.php
@@ -1,6 +1,5 @@
-<section class="divider<?php  echo (get_sub_field('colore_sfondo')=='red') ? ' divider--enphasis' : '' ?>">
-    <h2 class="divider__title title--shrink title--grow-md"><?php the_sub_field('titolo'); ?></h2>
-    <div class="divider__paragraph paragraph--shrink paragraph--grow-md"><?php the_sub_field('paragrafo'); ?></div>
-    <div class="divider__button button--shrink button--grow-md"><a href="<?php the_sub_field('link_pagina') ?>" class="<?php  echo (get_sub_field('colore_sfondo')=='red') ? ' button--reverse' : ' button' ?>"><?php the_sub_field('testo_bottone') ?></a>
-    </div>
+<section class="divider<?php  echo (get_sub_field('colore_sfondo')=='red') ? ' divider--enphasis' : '' ?>  divider--shrink divider--grow-lg">
+    <h2 class="divider__title"><?php the_sub_field('titolo'); ?></h2>
+    <div class="divider__paragraph paragraph--grow-lg"><?php the_sub_field('paragrafo'); ?></div>
+  <a href="<?php the_sub_field('link_pagina') ?>" class="<?php  echo (get_sub_field('colore_sfondo')=='red') ? ' button--reverse' : ' button' ?>"><?php the_sub_field('testo_bottone') ?></a>
 </section>
