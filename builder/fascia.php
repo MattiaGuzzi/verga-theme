01ac7902ba42e5f1f630c7dfa637869b3aa49a6f
Mattia <guzzi.mattia@gmail.com>
2017-02-16 12:03:17 +0100

    no message

--- builder/fascia.php
+++ builder/fascia.php
@@ -1,5 +1,5 @@
 <section class="divider">
-    <h2><?php the_field('titolo'); ?></h2>
-    <p><?php the_field('paragrafo'); ?></p>
-    <a href="<?php the_field('link_pagina') ?>"><?php the_field('testo_bottone') ?></a>
+    <h2><?php the_sub_field('titolo'); ?></h2>
+    <p><?php the_sub_field('paragrafo'); ?></p>
+    <a href="<?php the_sub_field('link_pagina') ?>"><?php the_field('testo_bottone') ?></a>
 </section>
