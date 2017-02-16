051170fa1edc4bdf344d7aa58393b6b1a4bab9ad
Mattia <guzzi.mattia@gmail.com>
2017-02-16 10:57:34 +0100

    no message

--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,12 @@
 <footer class="footer">
-  <div class="container">
-
-    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("footer left") ) : ?>
-    <?php endif;?>
-
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
-  <?php
-  if (has_nav_menu('footer_navigation')) :
-    bem_menu('footer_navigation', 'menu');
-  endif;
-  ?>
-
-
-  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("footer right") ) : ?>
-  <?php endif;?>
+    <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar("footer left")) : ?>
+    <?php endif; ?>
+    <?php /*dynamic_sidebar('sidebar-footer'); */ ?>
+    <?php
+    if (has_nav_menu('footer_navigation')) :
+        bem_menu('footer_navigation', 'menu');
+    endif;
+    ?>
+    <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar("footer right")) : ?>
+    <?php endif; ?>
 </footer>
