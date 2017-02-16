168fe2599611bfd52c4ef588625af2fcde2da5a3
Mattia <guzzi.mattia@gmail.com>
2017-02-16 12:27:18 +0100

    no message

--- builder/fascia.php
+++ builder/fascia.php
@@ -1,5 +1,5 @@
 <section class="divider<?php  echo (get_sub_field('colore_sfondo')=='red') ? ' divider--red' : '' ?>">
     <h2 class="divider__title title--shrink title--grow-md"><?php the_sub_field('titolo'); ?></h2>
     <p class="divider__paragraph paragraph--shrink paragraph--grow-md"><?php the_sub_field('paragrafo'); ?></p>
-    <a href="<?php the_sub_field('link_pagina') ?>" class="button--grow-md<?php  echo (get_sub_field('colore_sfondo')=='red') ? ' button' : ' button--reverse' ?>"><?php the_sub_field('testo_bottone') ?></a>
+    <a href="<?php the_sub_field('link_pagina') ?>" class="<?php  echo (get_sub_field('colore_sfondo')=='red') ? ' button' : ' button--reverse' ?>"><?php the_sub_field('testo_bottone') ?></a>
 </section>
