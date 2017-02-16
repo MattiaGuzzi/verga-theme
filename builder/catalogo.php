3156e4b359b9a1150cd632cafe5b6994f3013d98
Mattia <guzzi.mattia@gmail.com>
2017-02-16 16:37:58 +0100

    no message

--- builder/catalogo.php
+++ builder/catalogo.php
@@ -2,40 +2,13 @@
 <?php
 $args = array(
     'orderby'    => 'title',
-    'order'      => 'ASC',
+    'orderby' => 'name',
     'hide_empty' => 0,
     'hierarchical' => false,
 );
 $product_categories = get_terms( 'product_cat', $args );
 foreach ( $product_categories as $product_category ) {
-    echo '<h4><a href="' . get_term_link( $product_category ) . '">' . $product_category->name . '</a></h4>';
-    $args = array(
-        'posts_per_page' => -1,
-        'tax_query' => array(
-            'relation' => 'AND',
-            array(
-                'taxonomy' => 'product_cat',
-                'field' => 'slug',
-                // 'terms' => 'white-wines'
-                'terms' => $product_category->slug
-            )
-        ),
-        'post_type' => 'product',
-        'orderby' => 'title,'
-    );
-    $products = new WP_Query( $args );
-    echo "<ul>";
-    while ( $products->have_posts() ) {
-        $products->the_post();
-        ?>
-        <li>
-            <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-            </a>
-        </li>
-        <?php
-    }
-    echo "</ul>";
+   echo $product_categorie -> name;
 }
 ?>
 </section>
