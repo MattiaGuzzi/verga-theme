3ab92dd88afabee4ca7006b4c7a65caaf8c48b7d
Mattia <guzzi.mattia@gmail.com>
2017-02-17 10:21:40 +0100

    no message

--- builder/catalogo.php
+++ builder/catalogo.php
@@ -25,7 +25,7 @@
                     echo $subcategory->name;
                 }
                ?>
-                <a href="http://verga.dnative.com/preventivo/" class="block__button button--rotate"><label class="button__label"><?php _e('Scopri tutto','verga') ?>/label></a>
+                <a href="http://verga.dnative.com/preventivo/" class="block__button button button--rotate"><label class="button__label"><?php _e('Scopri tutto','verga') ?></label></a>
             </div>
         </div>
     <?php
