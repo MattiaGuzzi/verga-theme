9bf46151e6b1ba2751beb629a32046c5796531cb
Mattia <guzzi.mattia@gmail.com>
2017-02-17 18:12:51 +0100

    no message

--- builder/colonne.php
+++ builder/colonne.php
@@ -1,7 +1,7 @@
 <?php $wcols = 12/get_sub_field('n_cols') ?>
-<section class="section section--grid section--shrink section--grow-lg">
+<section class="section section--grid">
     <?php while (have_rows('cols')) : the_row(); ?>
-    <div class="section__cell section__cell-s<?php echo $wcols?>">
+    <div class="section__cell section__cell-s<?php echo $wcols?>  section__cell--shrink section__cell--grow-lg">
         <?php if(get_sub_field('immagine') != '') :?>
             <div class="section__cell--background" style="background-image: url('<?php the_sub_field('immagine') ?>')"></div>
         <?php endif ?>
@@ -9,7 +9,7 @@
             <h2 class="section__cell--title"><?php the_sub_field('titolo') ?></h2>
         <?php endif ?>
         <?php if(get_sub_field('paragrafo') != '') :?>
-            <p class="section__cell--paragraph section__cell--paragraph--grow-lg"><?php the_sub_field('paragrafo') ?></p>
+            <div class="section__cell--paragraph section__cell--paragraph--grow-lg"><?php the_sub_field('paragrafo') ?></div>
         <?php endif ?>
         <?php if(get_sub_field('link') != '') :?>
             <a href="<?php the_sub_field('link') ?>" class="button section__cell--button"><span class="button__label"><?php the_sub_field('testo_bottone') ?></span></a>
