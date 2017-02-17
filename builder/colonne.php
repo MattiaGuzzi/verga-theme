24cc87abeb5b52ccb8090149bfe0aa269f112d1d
Mattia <guzzi.mattia@gmail.com>
2017-02-17 17:29:17 +0100

    no message

--- builder/colonne.php
+++ builder/colonne.php
@@ -2,14 +2,14 @@
 <section class="section section--grid">
     <?php while (have_rows('cols')) : the_row(); ?>
     <div class="section__cell section__cell-s<?php echo $wcols?>">
-        <?php if(get_sub_field('n_cols') != '') :?>
+        <?php if(get_sub_field('immagine') != '') :?>
             <div class="section__cell--background" style="background-image: url("<?php the_sub_field('immagine') ?>")"></div>
         <?php endif ?>
         <?php if(get_sub_field('titolo') != '') :?>
-            <h2 class="section__cell--title><?php the_sub_field('titolo') ?></h2>
+            <h2 class="section__cell--title"><?php the_sub_field('titolo') ?></h2>
         <?php endif ?>
         <?php if(get_sub_field('paragrafo') != '') :?>
-            <h2 class="section__cell--paragraph><?php the_sub_field('paragrafo') ?></h2>
+            <p class="section__cell--paragraph"><?php the_sub_field('paragrafo') ?></p>
         <?php endif ?>
         <?php if(get_sub_field('link') != '') :?>
             <a href="<?php the_sub_field('link') ?>" class="button section__cell--button"><span class="button__label"><?php the_sub_field('testo_bottone') ?></span></a>
