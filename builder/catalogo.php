6536512f10f4766959e4260654ab18839310e8ab
Mattia <guzzi.mattia@gmail.com>
2017-02-17 12:33:57 +0100

    no message

--- builder/catalogo.php
+++ builder/catalogo.php
@@ -8,30 +8,31 @@
     $product_categories = get_terms('product_cat', $args);
     foreach ($product_categories as $product_category) {
         $thumbnail_id = get_woocommerce_term_meta($product_category->term_id, 'thumbnail_id', true);
-        $image = wp_get_attachment_image_src($thumbnail_id, 'full')[0];?>
+        $image = wp_get_attachment_image_src($thumbnail_id, 'full')[0]; ?>
 
-        <div class="catalog__item" style="background-image: url('<?php echo $image  ?>')">
+        <div class="catalog__item" style="background-image: url('<?php echo $image ?>')">
             <div class="block block--grow-lg block--shrink">
-                <h2 class="block__title block__title--grow-md-top"><?php echo $product_category->name;?></h2>
-                <p class="block__description block__description--grow-md"><?php echo $product_category->description;?></p>
+                <h2 class="block__title block__title--grow-md-top"><?php echo $product_category->name; ?></h2>
+                <p class="block__description block__description--grow-md"><?php echo $product_category->description; ?></p>
                 <div class="block__submenu">
-                <?php
-                $args = array(
-                    'orderby' => 'name',
-                    'hide_empty' => 0,
-                    'parent' => $product_category->term_id,
-                );
-                $subcategories = get_terms('product_cat', $args);
-                foreach ($subcategories as $subcategory) {
-                    echo '<span class="item"><a href="'.get_category_link( $subcategory -> term_id ).'">'.$subcategory->name.'</a></span>';
-                }
-               ?>
+                    <?php
+                    $args = array(
+                        'orderby' => 'name',
+                        'hide_empty' => 0,
+                        'parent' => $product_category->term_id,
+                    );
+                    $subcategories = get_terms('product_cat', $args);
+                    foreach ($subcategories as $subcategory) {
+                        echo '<span class="item"><a href="' . get_category_link($subcategory->term_id) . '">' . $subcategory->name . '</a></span>';
+                    }
+                    ?>
                 </div>
-                <a href="http://verga.dnative.com/preventivo/" class="block__button button button--rotate"><label class="button__label"><?php _e('Scopri tutto','verga') ?></label></a>
-
+                <a href="http://verga.dnative.com/preventivo/" class="block__button button button--rotate"><label
+                        class="button__label"><?php _e('Scopri tutto', 'verga') ?></label></a>
+                <div class="gray-overlay"></div>
             </div>
-            </div>
-    <?php
+        </div>
+        <?php
     }
     ?>
 </section>
