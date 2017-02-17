7f6056914a648a690446ab0a073d479c691840c2
Mattia <guzzi.mattia@gmail.com>
2017-02-17 17:15:28 +0100

    no message

--- builder/colonne.php
+++ builder/colonne.php
@@ -1,5 +1,6 @@
 <?php $wcols = 12/get_sub_field('n_cols') ?>
 <section class="section section--grid">
+    <?php while (have_rows('cols')) : the_row(); ?>
     <div class="section__cell section__cell-s<?php echo $wcols?>"></div>
-
+    <?php endwhile; ?>
 </section>
