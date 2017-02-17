3ec8f8e61b542e5707c054e65526ae79998f2a67
Mattia <guzzi.mattia@gmail.com>
2017-02-17 12:48:19 +0100

    no message

--- builder/catalogo.php
+++ builder/catalogo.php
@@ -8,7 +8,7 @@
     $product_categories = get_terms('product_cat', $args);
     foreach ($product_categories as $product_category) {
         $thumbnail_id = get_woocommerce_term_meta($product_category->term_id, 'thumbnail_id', true);
-        $image = wp_get_attachment_image_src($thumbnail_id, 'full')[0]; var_dump($product_category) ?>
+        $image = wp_get_attachment_image_src($thumbnail_id, 'full')[0]; echo $product_category->count ?>
 
         <div class="catalog__item" style="background-image: url('<?php echo $image ?>')">
             <div class="block block--grow-lg block--shrink">
