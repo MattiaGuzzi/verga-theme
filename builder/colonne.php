c315739c19ffa1af029dd674286107c5dd8a18c0
Mattia <guzzi.mattia@gmail.com>
2017-02-17 17:11:47 +0100

    no message

--- builder/colonne.php
+++ builder/colonne.php
@@ -1,6 +1,5 @@
 <?php $wcols = 12/get_sub_field('n_cols') ?>
-<?php var_dump($wcols) ?>
 <section class="section section--grid">
-    <div class="section__cell section__cell-s<?php echo $wcols?>6"></div>
+    <div class="section__cell section__cell-s<?php echo $wcols?>"></div>
 
 </section>
