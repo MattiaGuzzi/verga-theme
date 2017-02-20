9d52014fac7de2bd94b6c9ede67fcfa1d1318d7b
Mattia <guzzi.mattia@gmail.com>
2017-02-20 16:57:10 +0100

    no message

--- lib/extras.php
+++ lib/extras.php
@@ -58,4 +58,5 @@ if ( function_exists('register_sidebar') )
 // Remove the product rating display on product loops
 remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
 
+remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
 
