9900341307f523e60664a5adb2a2755fbcfe03e3
Mattia <guzzi.mattia@gmail.com>
2017-02-20 15:18:34 +0100

    no message

--- templates/header.php
+++ templates/header.php
@@ -15,14 +15,14 @@
             <label class="button__label"><?php _e('Preventivo', 'verga') ?></label>
         </a>
     </div>
-    <div class="mainmenu">
-        <div class="mainmenu__header  mainmenu__header--shrink">
-            <div class="mainmenu__header--title"><?php _e('catalogo', 'verga') ?></div>
-            <div class="mainmenu__header--button mainmenu__header--button-close"><a href="#" class="button">
+    <div class="main-menu">
+        <div class="main-menu__header  main-menu__header--shrink">
+            <div class="main-menu__header--title"><?php _e('catalogo', 'verga') ?></div>
+            <div class="main-menu__header--button main-menu__header--button-close"><a href="#" class="button">
                     <label class="button__label"><?php _e('Chiudi', 'verga') ?></label>
                 </a></div>
         </div>
-        <ul class="mainmenu__left">
+        <ul class="main-menu__left">
             <?php   $args = array(
                 'orderby' => 'name',
                 'hide_empty' => 0,
@@ -30,11 +30,12 @@
             );
             $product_categories = get_terms('product_cat', $args);
             foreach ($product_categories as $product_category) {?>
-            <li class="mainmenu__left--list mainmenu__left--shrink" data-attribute="<?php echo $product_category->name; ?>"><a href="<?php esc_url( get_term_link( $product_category ) ) ?>"><span><?php echo $product_category->name; ?></span></a></li>
+                <?php $link = get_terms_link( (int)$product_category->term_id, 'product_cat' ) ?>
+            <li class="main-menu__left--list main-menu__left--shrink" data-attribute="<?php echo $product_category->name; ?>"><a href="<?php  echo $link ?>"><span><?php echo $product_category->name; ?></span></a></li>
             <?php } ?>
         </ul>
 
-        <div class="mainmenu__right">
+        <div class="main-menu__right">
         <?php   $args = array(
             'orderby' => 'name',
             'hide_empty' => 0,
@@ -42,7 +43,7 @@
         );
         $product_categories = get_terms('product_cat', $args);
         foreach ($product_categories as $product_category) {?>
-        <div class="mainmenu__right--list" data-attribute="<?php echo $product_category->name; ?>">
+        <div class="main-menu__right--list" data-attribute="<?php echo $product_category->name; ?>">
             <?php
             $args = array(
                 'orderby' => 'name',
