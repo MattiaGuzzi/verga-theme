d25d565fe4568e554344b0894d0acff005baa577
Mattia <guzzi.mattia@gmail.com>
2017-02-21 14:49:39 +0100

    no message

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -12,7 +12,7 @@ remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_l
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 
 function item_before_img (){
-    ?> <span><span class="button">
+    ?> <span class="item-thumbnail"><span class="button">
         <span class="button__label"><?php _e('scopri', 'verga'); ?></span>
     </span>
 <?php
