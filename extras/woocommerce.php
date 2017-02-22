3f711d1486f06637da0db544e28e4dc526141321
Mattia <guzzi.mattia@gmail.com>
2017-02-22 12:10:50 +0100

    no message

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -28,6 +28,7 @@ add_action( 'woocommerce_before_shop_loop_item_title', 'close_tag', 11);
 
 function header_title () {
     global $product;
+    var_dump($product);
     echo '<div class="header-title">';
     echo '<div class="product-parent">' . $product->get_parent(). '</div>';
     if ($product->get_sku()) {
