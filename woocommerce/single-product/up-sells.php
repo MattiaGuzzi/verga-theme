f538431a53b3b2e1e31250e14860a17d1ce5834b
Mattia <guzzi.mattia@gmail.com>
2017-02-22 18:06:30 +0100

    no message

--- woocommerce/single-product/up-sells.php
+++ woocommerce/single-product/up-sells.php
@@ -45,7 +45,7 @@ if ($products->have_posts()) : ?>
 
     <div class="up-sells upsells products upsells--shrink">
 
-        <!--<h2><?php /*_e('You may also like&hellip;', 'woocommerce') */?></h2>-->
+        <h2><?php _e('You may also like&hellip;', 'woocommerce') ?></h2>
 
         <?php woocommerce_product_loop_start(); ?>
 
