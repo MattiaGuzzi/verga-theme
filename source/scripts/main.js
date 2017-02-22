388833eb4536cab49c9893b271239ca7471a5cb6
Mattia <guzzi.mattia@gmail.com>
2017-02-22 17:59:23 +0100

    no message

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -126,7 +126,20 @@
 
   function initCarousel() {
     $('.carousel_up-sells').owlCarousel({
-
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:3
+        },
+        1000:{
+          items:5
+        }
+      }
     })
   }
 
