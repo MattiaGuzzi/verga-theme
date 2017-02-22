4e00afa2f23beca9ba91ec8eda5d047bcb01925d
Mattia <guzzi.mattia@gmail.com>
2017-02-22 16:48:53 +0100

    no message

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -53,6 +53,9 @@ remove_action('woocommerce_single_product_summary','woocommerce_template_single_
 remove_action('woocommerce_single_product_summary','woocommerce_template_single_rating', 10);
 
 remove_action('woocommerce_single_product_summary','woocommerce_template_single_meta', 40);
+
+remove_action('woocommerce_after_single_product_summary','woocommerce_output_related_products', 15);
+remove_action('woocommerce_after_single_product_summary','woocommerce_upsell_display', 20);
 add_action('woocommerce_after_single_product_summary','woocommerce_output_related_products', 20);
 add_action('woocommerce_after_single_product_summary','woocommerce_upsell_display', 15);
 
