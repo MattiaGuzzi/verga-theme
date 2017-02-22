aeb73d3874c40e7e7c2113df7045a0393bc392eb
Mattia <guzzi.mattia@gmail.com>
2017-02-22 17:56:27 +0100

    no message

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -28,7 +28,7 @@ if (empty($product) || !$product->is_visible()) {
 }
 ?>
 
-<li <?php post_class('product__cell-s3 carousel_item'); ?>>
+<li <?php post_class('product__cell-s3'); ?>>
     <?php
     /**
      * woocommerce_before_shop_loop_item hook.
