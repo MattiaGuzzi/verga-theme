33d278e3a370ec2eaa08680ac385029a6799a1bf
Mattia <guzzi.mattia@gmail.com>
2017-02-22 11:25:35 +0100

    no message

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -33,3 +33,16 @@ remove_action('woocommerce_single_product_summary','woocommerce_template_single_
 remove_action('woocommerce_single_product_summary','woocommerce_template_single_add_to_cart', 30);
 remove_action('woocommerce_single_product_summary','woocommerce_template_single_meta', 40);
 
+
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+
+function woo_remove_product_tabs( $tabs ) {
+
+    unset( $tabs['description'] );      	// Remove the description tab
+    unset( $tabs['reviews'] ); 			// Remove the reviews tab
+    unset( $tabs['additional_information'] );  	// Remove the additional information tab
+
+    return $tabs;
+
+}
+
