cb55e114cbedafc5bbe615c630348711ab4831a3
Mattia <guzzi.mattia@gmail.com>
2017-02-23 15:51:08 +0100

    no message

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -85,28 +85,22 @@ if ( post_password_required() ) {
 
 
 <section class="catalog">
-	<?php
-	$args = array(
-		'orderby' => 'name',
-		'hide_empty' => 0,
-		'parent' => 0,
-
-	);
-	$product_categories = get_terms('product_cat', $args);
-	foreach ($product_categories as $product_category) {
-		global $product;
+	<?php global $product;
 		$id = $product->id;
-		$thumbnail_id = get_woocommerce_term_meta( wp_get_post_parent_id( $id ), 'thumbnail_id', true);
-		$image = wp_get_attachment_image_src($thumbnail_id, 'full')[0];?>
+		var_dump($id);
+		var_dump(wp_get_post_parent_id( $id ));
+
+		/*$thumbnail_id = get_woocommerce_term_meta( wp_get_post_parent_id( $id ), 'thumbnail_id', true);
+		$image = wp_get_attachment_image_src($thumbnail_id, 'full')[0];*/?><!--
 
 		<div class="catalog__item">
-			<div class="background" style="background-image: url('<?php echo $image ?>')"></div>
+			<div class="background" style="background-image: url('<?php /*echo $image */?>')"></div>
 			<div class="block block--grow-lg block--shrink">
-				<h2 class="block__title block__title--grow-md-top"><?php echo $product_category->name; ?></h2>
-				<p class="block__description block__description--grow-md"><?php echo $product_category->description; ?></p>
+				<h2 class="block__title block__title--grow-md-top"><?php /*echo $product_category->name; */?></h2>
+				<p class="block__description block__description--grow-md"><?php /*echo $product_category->description; */?></p>
 				<div class="block__submenu">
 					<?php
-					$args = array(
+/*					$args = array(
 						'orderby' => 'name',
 						'hide_empty' => 0,
 						'parent' => $product_category->term_id,
@@ -115,16 +109,13 @@ if ( post_password_required() ) {
 					foreach ($subcategories as $subcategory) {
 						echo '<span class="item"><a href="' . get_category_link($subcategory->term_id) . '">' . $subcategory->name . '</a></span>';
 					}
-					?>
+					*/?>
 				</div>
 				<div class="block__button">
-					<a href="<?php get_term_link( $product_category ); ?>" class="button button--rotate"><span
-							class="button__label"><?php _e('Scopri tutto', 'verga') ?></span></a>
+					<a href="<?php /*get_term_link( $product_category ); */?>" class="button button--rotate"><span
+							class="button__label"><?php /*_e('Scopri tutto', 'verga') */?></span></a>
 				</div>
-				<div class="block__overlay"><p class="totale"><?php echo $product_category->count; _e(" prodotti","verga")?></p></div>
+				<div class="block__overlay"><p class="totale"><?php /*echo $product_category->count; _e(" prodotti","verga")*/?></p></div>
 			</div>
-		</div>
-		<?php
-	}
-	?>
+		</div>-->
 </section>
