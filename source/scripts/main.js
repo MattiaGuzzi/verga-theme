2557a4e9ed20e8957900cb5efdc935ff419d83a4
Mattia <guzzi.mattia@gmail.com>
2017-02-27 15:09:25 +0100

    no message

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -26,6 +26,7 @@
                 ajaxPostPerPage();
                 initCarousel();
                 checkAut();
+                mainMenu();
             },
             finalize: function () {
                 // JavaScript to be fired on all pages, after page specific JS is fired
